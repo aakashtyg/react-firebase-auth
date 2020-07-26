d53fa29d422de0214cf0156e6f0c34cde88e966d
Aakash Tyagi <aakash.tyg93@gmail.com>
2020-07-26 23:16:47 +0530

    Project setup with firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBuCCySsLe2tscrSw9Q07kupzeDDd2hcKQ',
+  authDomain: 'react-firebase-auth-392e7.firebaseapp.com',
+  databaseURL: 'https://react-firebase-auth-392e7.firebaseio.com',
+  projectId: 'react-firebase-auth-392e7',
+  storageBucket: 'react-firebase-auth-392e7.appspot.com',
+  messagingSenderId: '211194483143',
+  appId: '1:211194483143:web:61234996eca655496609e2',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
